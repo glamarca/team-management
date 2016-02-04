0cd8589d379bd07c7da045e3288a456d8e33cf48
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2016-02-04 14:10:03 +0100

    renaming

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
-from team_management import settings
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "team_management.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TeamManagement.settings")
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
